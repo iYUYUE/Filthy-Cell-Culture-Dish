0f976ec4390ec0b94ad60a0f1ae46271f1f22b76
ylns1314 <ylns1314@gmail.com>
2015-06-04 03:31:50 +0800

    DPI based Demographic with no X button

---
+++ FCCD Unity Project/Assets/Scripts/GUI/SwitchDemo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwitchDemo : MonoBehaviour {
+	Button demoButton;
+	[SerializeField] private Canvas demoCanvas;
+	// Use this for initialization
+	void Start () {
+		demoButton = GetComponent<Button> ();
+		demoButton.onClick.AddListener(() => { demoCanvas.enabled = !demoCanvas.enabled; });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
