0d150fc836f5f50c19e2b5bff2896e1af10dc4c4
ChunchuanLv <chunchuan.lv@gmail.com>
2015-06-04 01:06:25 +0800

    Color

--- FCCD Unity Project/Assets/Scripts/Mechanics/Nexturn.cs
+++ FCCD Unity Project/Assets/Scripts/Mechanics/Nexturn.cs
@@ -4,14 +4,15 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using AssemblyCSharp;
 public class Nexturn : MonoBehaviour {
-
+	
 	[SerializeField] private Button MyButton = null; // assign in the editor
 	
 	void Start() { MyButton.onClick.AddListener(() => { nextTurn();  });
 	}
-
-
+	
+	
 	public void nextTurn(){
+		Debug.Log(Global.numberOfPlayers);
 		if (!Global.explored||Global.players[Global.currentPlayer].researhDone()) 
 			return;
 		Global.currentPlayer = (Global.currentPlayer + 1)% Global.numberOfPlayers;
@@ -20,9 +21,8 @@ public class Nexturn : MonoBehaviour {
 			Global.updateDemo();
 			Global.updateWinStatus();
 		}
-
-		Global.explored = false;
-		Debug.Log(Global.numberOfPlayers);
-
+		
+		Global.explored = true;
+		
 	}
 }
