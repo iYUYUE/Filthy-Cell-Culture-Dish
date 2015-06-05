38e56ed6110d2553d9de1b98d16b056a2da37d94
ylns1314 <ylns1314@gmail.com>
2015-06-05 15:37:17 +0800

    Player indicator added.
    by changing the color of NextTurn Button

---
+++ FCCD Unity Project/Assets/Scripts/GUI/playerIndicator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+namespace AssemblyCSharp
+{
+
+	public class playerIndicator : MonoBehaviour {
+		public Button nextTurn;
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			nextTurn.image.color = Global.players [Global.currentPlayer].getColor ();
+		}
+	}
+
+}
