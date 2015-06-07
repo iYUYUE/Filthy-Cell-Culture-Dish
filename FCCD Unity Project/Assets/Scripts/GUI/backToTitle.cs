0246aed34fd05b54a7693223fa2ce8a7393033ea
ylns1314 <ylns1314@gmail.com>
2015-06-07 15:04:58 +0800

    Build successfully.

--- FCCD Unity Project/Assets/Scripts/GUI/backToTitle.cs
+++ FCCD Unity Project/Assets/Scripts/GUI/backToTitle.cs
@@ -12,6 +12,7 @@ public class backToTitle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		quitBtn.onClick.AddListener (() => {
+			//Application.LoadLevel("Start");
 			Application.Quit();
 		});
 	}
